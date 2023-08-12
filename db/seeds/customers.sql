fbd292b76cd2aecde07a70ea77996f2a879ac3f4
Douglas Modena <d.modena@live.com>
2023-08-12 18:46:26 -0300

    Fix casing in customers and stays_v1 seeds

--- db/seeds/customers.sql
+++ db/seeds/customers.sql
@@ -1,4 +1,4 @@
-INSERT INTO dbo.customers values
+INSERT INTO dbo.customers VALUES
 ('0a05782e-b5e7-47a9-b94e-62a0f6d2af05', 'John Doe'),
 ('a3a97187-30eb-48f9-9dff-d1d670495e65', 'Jane Doe'),
 ('2b73d1cd-8756-49f3-a977-d1a2de021445', 'Mary Smith'),
