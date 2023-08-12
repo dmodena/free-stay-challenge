be7b1a0f80f3b1fc6c143445db6b3ecf561aea7b
Douglas Modena <d.modena@live.com>
2023-08-12 00:25:01 -0300

    Add customer without stays on stays_v1 to seed

--- db/seeds/customers.sql
+++ db/seeds/customers.sql
@@ -1,4 +1,5 @@
 INSERT INTO dbo.customers values
 ('0a05782e-b5e7-47a9-b94e-62a0f6d2af05', 'John Doe'),
 ('a3a97187-30eb-48f9-9dff-d1d670495e65', 'Jane Doe'),
-('2b73d1cd-8756-49f3-a977-d1a2de021445', 'Mary Smith');
+('2b73d1cd-8756-49f3-a977-d1a2de021445', 'Mary Smith'),
+('2ed10ec4-bdcb-422c-9533-cd4944d661b8', 'Mark Smith');
