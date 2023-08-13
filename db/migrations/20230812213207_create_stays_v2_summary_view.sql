37beec4c93a7b815679bbb0da033c718fbc0284b
Douglas Modena <d.modena@live.com>
2023-08-12 22:12:20 -0300

    Fix casing in stays_v2_summary migration

--- db/migrations/20230812213207_create_stays_v2_summary_view.sql
+++ db/migrations/20230812213207_create_stays_v2_summary_view.sql
@@ -4,12 +4,12 @@ SELECT sq.customer_id,
     sq.stay_days,
     sq.free_stays,
     (SELECT CASE WHEN (sq.period_check_in IS NULL OR sq.period_check_out IS NULL) THEN 0
-        ELSE (sq.period_check_out - sq.period_check_in) end) as period_stays
+        ELSE (sq.period_check_out - sq.period_check_in) END) AS period_stays
 FROM
 (SELECT s.customer_id,
     s.check_in,
     s.check_out,
-    (SELECT s.check_out - s.check_in) as stay_days,
+    (SELECT s.check_out - s.check_in) AS stay_days,
     s.free_stays,
     (SELECT CASE WHEN s.check_out < DATE(current_date - '1 year'::interval) THEN NULL
         ELSE GREATEST(DATE(current_date - '1 year'::interval), s.check_in) END) AS period_check_in,
