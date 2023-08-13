d416dcc11305123737de4a38770d3d751dc204c9
Douglas Modena <d.modena@live.com>
2023-08-12 22:08:04 -0300

    Make stays_v2 FK column not nullable

--- db/migrations/20230812110409_create_stays_v2_table.sql
+++ db/migrations/20230812110409_create_stays_v2_table.sql
@@ -1,6 +1,6 @@
 -- migrate:up
 CREATE TABLE dbo.stays_v2 (
-    customer_id uuid REFERENCES dbo.customers(id),
+    customer_id uuid NOT NULL REFERENCES dbo.customers(id),
     check_in DATE NOT NULL,
     check_out DATE NOT NULL,
     free_stays NUMERIC NOT NULL DEFAULT 0
